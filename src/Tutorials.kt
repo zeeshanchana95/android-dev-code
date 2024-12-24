3edbedfcb5dec2cbacdfbecfb3468a2e3290f30e
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-24 15:50:39 +0500

    Hello World program in Kotlin

---
+++ src/Tutorials.kt
@@ -0,0 +1,8 @@
+import java.io.IO.println
+
+fun main() {
+    println("Welcome to the world of Android Development")
+    println("This is my first program in this journey.")
+    println("Good Luck ")
+
+}
