6422c7a477d7ba9da377183abe32c250645884c9
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-25 23:23:36 +0500

    for loop in Kotlin

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -2,59 +2,42 @@ import java.io.IO.print
 import java.io.IO.println
 
 fun main() {
-    //while loop: to execute certain pieces of code several time
-
-    var x = 3;
-    while (x > 0) {
-        println("hello")
-        x--
-    }
-    println("whats up?")
-
-    //printing array elements
-    val myArray = arrayOf("hello", "guys", "whats up?")
-    val arrayLength = myArray.size
-    var i = 0
-    while (i < arrayLength) {
-        println(myArray[i])
-        i++
+    //for loop:
+    //use specific array elements
+    val myArray = arrayOf(1,2,3)
+    for(i in myArray) {
+        println(i)
     }
 
+    //use range
+    for( i in 5..10) {
+        println(i)
+    }
 
-    //practice task 01: input number and print count down
-    println("Enter number: ")
-    val number = readLine()?.toInt()
-    println("Lets count from $x down to 0:")
-    if(number != null) {
-        while(number >= 0) {
-            println(number)
-            number--
+    //count down
+    for(i in 10 downTo 5){
+        println(i)
+    }
+
+    //count down with 2 difference
+    for(i in 10 downTo 5 step 2){
+        println(i)
+    }
+
+    //print letters from a to z
+    for(i in 'a'..'z') {
+        println(i)
+    }
+
+
+    //practical example
+    val myArray1 = arrayOf((4,5,8,9,10,2,3)
+    var max = myArray1[0]
+    for(item in myArray1) {
+        if(item > max) {
+            max = item
         }
     }
-
-    //practice task 02: enter two numbers and calculate the result by first number power of second number
-    println("Enter number 01: ")
-    val num1 = readLine()?.toInt()
-    println("Enter number 02: ")
-    val num2 = readLine()?.toInt()
-
-    if(num1 != null && num2 != null) {
-        var result = 1
-        var i = 0
-        while(i < num2) {
-            result *= num1
-            i++
-        }
-        println("$num1 to the power of $num2 is $result")
-    }
-
-
-
-
-
-
-
-
-
+    println("Maximum value of array is $max")
 
 }
