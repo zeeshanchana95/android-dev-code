101d931e9936ffb5176e84348dc1d0970482ecee
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-25 23:14:45 +0500

    while loop in Kotlin

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,24 +1,59 @@
 import java.io.IO.print
 import java.io.IO.println
 
-
 fun main() {
-    //array: ways to save several objects inside single object
-//    val myIntArray = arrayOf(3, 2, 6, 4)
-//    val myMixArray = arrayOf("hello", "guys", 3)
+    //while loop: to execute certain pieces of code several time
 
-    val first = "hello"
-    val second = "guys"
-    val third = "whats up?"
-    //above and below line is equal
-    val myStringArray = arrayOf("hello", "guys", "what's up")
-    println(myStringArray) //can't print array with println method
+    var x = 3;
+    while (x > 0) {
+        println("hello")
+        x--
+    }
+    println("whats up?")
 
-    //we can access individual elements of array using index
-    println(myStringArray[0]) //hello (first element)
-    println(myStringArray[1])
-    println(myStringArray[2])
-    println(myStringArray[3]) //Exception: ArrayIndexOutOfBound
+    //printing array elements
+    val myArray = arrayOf("hello", "guys", "whats up?")
+    val arrayLength = myArray.size
+    var i = 0
+    while (i < arrayLength) {
+        println(myArray[i])
+        i++
+    }
+
+
+    //practice task 01: input number and print count down
+    println("Enter number: ")
+    val number = readLine()?.toInt()
+    println("Lets count from $x down to 0:")
+    if(number != null) {
+        while(number >= 0) {
+            println(number)
+            number--
+        }
+    }
+
+    //practice task 02: enter two numbers and calculate the result by first number power of second number
+    println("Enter number 01: ")
+    val num1 = readLine()?.toInt()
+    println("Enter number 02: ")
+    val num2 = readLine()?.toInt()
+
+    if(num1 != null && num2 != null) {
+        var result = 1
+        var i = 0
+        while(i < num2) {
+            result *= num1
+            i++
+        }
+        println("$num1 to the power of $num2 is $result")
+    }
+
+
+
+
+
+
+
 
 
 
