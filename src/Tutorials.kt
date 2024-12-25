0870967e2031e472c5e300ed57a5020ccfbac4be
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-25 15:09:34 +0500

    strings in kotlin

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,18 +1,13 @@
 import java.io.IO.println
 
-fun main() {
-    var x = 20.123235F //float (32 bits) memory but less accurate
-    var y = 8F
-    println("The result of x / y is: ${x / y}")
 
-    var x1 = 20.1534334344334 //for double (64 bits) but more accurate
-    var y1 = 8
-    println("The result of x / y is: ${x / y}")
+fun main() {
+   val string = "This is an example text"
+    println("Our string is: ${string.toUpperCase()}")
+    println("Our string is: ${string.toUpperCase().toLowerCase()}")
 
     //practice task
-    //calculating volume of sphere
-    val PI = 3.142
-    var radius = 5.5
-    var volumeOfSphere = 0.75 * PI * (radius*radius*radius)
-    println("The volume of the sphere with the radius $radius is $volumeOfSphere")
+    val name:String = "Zeeshan"
+    println("My name in uppercase and reversed is: ${name.toUpperCase().reversed()}")
+
 }
