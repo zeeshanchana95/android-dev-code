fc30e3cf0467541c6c0d8e36b5ecaa8f5ae3aeb1
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-26 10:35:30 +0500

    lists in Kotlin

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -2,42 +2,77 @@ import java.io.IO.print
 import java.io.IO.println
 
 fun main() {
-    //for loop:
-    //use specific array elements
-    val myArray = arrayOf(1,2,3)
-    for(i in myArray) {
-        println(i)
-    }
+   //lists in kotlin
+    //arrays values can be changed but lists value can't be change
+    val array = arrayOf(1,2,3) //arrays size can't be changed
+//    val list = listof(1,2,3)
 
-    //use range
-    for( i in 5..10) {
-        println(i)
-    }
+    array[0] = 3
+//    list[0] = 3 //values of lists are immutable means can't changed
 
-    //count down
-    for(i in 10 downTo 5){
-        println(i)
-    }
+    //how to make mutable list?
+    //using mutable function
+    val list = mutableListof(1,2,3) //mutable list can change even in size
+    array[0] = 3
+    list[0] = 3
 
-    //count down with 2 difference
-    for(i in 10 downTo 5 step 2){
-        println(i)
-    }
+    list.add(4) //add new item in list at runtime
+    println(list) //print the whole list i.e., [1,2,3,4]
 
-    //print letters from a to z
-    for(i in 'a'..'z') {
-        println(i)
-    }
+    //remove value at the end from the list
+    list.remove(4)
+    println(list) //[1,2,3]
+
+    //remove value at particular index
+    list.removeAt(2)
+    println(list)
+
+    //so, if we have to add values at the end, we need to use mutable list
 
 
-    //practical example
-    val myArray1 = arrayOf((4,5,8,9,10,2,3)
-    var max = myArray1[0]
-    for(item in myArray1) {
-        if(item > max) {
-            max = item
+
+    //practice task: task 10 numbers as input from user and then print the list at the end
+    val mutableList = mutableListOf<Int>()
+    println("Enter 10 numbers one by one to add in the list: ")
+    for(i in 1..10) {
+        val x = readLine()?.toInt()
+        if(x != null){
+            mutableList.add(x)
         }
     }
-    println("Maximum value of array is $max")
+    //can also use for loop to print list items
+//    for(i in mutableList) {
+//        println(i)
+//    }
+    println(mutableList)
+
+
+
+    //practice task: input 5 numbers from user and print them in reverse order
+    val mutableList2 = mutableListOf<Int>()
+    println("Enter 5 numbers one by one: ")
+    for(i in 1..5) {
+        val number = readLine()?.toInt()
+        if(number != null) {
+            mutableList2.add(number)
+        }
+    }
+    for(i in mutableList2.size downTo 1) {
+        println(mutableList2[i])
+    }
+
+
+    //practice task: input number larger than 1 and then generate fibonacci series up to that number
+    val mutableList3 = mutableListOf(0, 1)
+    println("Enter a number n > 1: ")
+    val number = readLine()?.toInt()
+    if(number != null) {
+        var nextNumber = 0
+        for(i in 1..number-2) {
+            nextNumber = mutableList3[i-1] + mutableList3[i-2]
+            mutableList3.add(nextNumber)
+        }
+    }
+    println(mutableList3)
 
 }
