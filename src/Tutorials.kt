3979d2c2d5fbf83273207121198a42c36f98dfa4
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-26 11:22:25 +0500

    when expression in Kotlin with practice task and solutions

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -2,77 +2,70 @@ import java.io.IO.print
 import java.io.IO.println
 
 fun main() {
-   //lists in kotlin
-    //arrays values can be changed but lists value can't be change
-    val array = arrayOf(1,2,3) //arrays size can't be changed
-//    val list = listof(1,2,3)
-
-    array[0] = 3
-//    list[0] = 3 //values of lists are immutable means can't changed
-
-    //how to make mutable list?
-    //using mutable function
-    val list = mutableListof(1,2,3) //mutable list can change even in size
-    array[0] = 3
-    list[0] = 3
-
-    list.add(4) //add new item in list at runtime
-    println(list) //print the whole list i.e., [1,2,3,4]
-
-    //remove value at the end from the list
-    list.remove(4)
-    println(list) //[1,2,3]
-
-    //remove value at particular index
-    list.removeAt(2)
-    println(list)
-
-    //so, if we have to add values at the end, we need to use mutable list
-
-
-
-    //practice task: task 10 numbers as input from user and then print the list at the end
-    val mutableList = mutableListOf<Int>()
-    println("Enter 10 numbers one by one to add in the list: ")
-    for(i in 1..10) {
-        val x = readLine()?.toInt()
-        if(x != null){
-            mutableList.add(x)
-        }
+    //when statement
+        //use to simply if statements
+        //it takes less line of code
+        //simplify statements
+    val age = readLine()?.toInt()
+    val x = 5
+    when(age) {
+        in 0..5 -> println("You're a young kid.")
+        in 6..17 -> println("You're a teenager.")
+        18 -> println("Finally, you're 18")
+        19, 20 -> println("You're a young adult.")
+        in 21..65 -> println("YOu're an adult.")
+        else -> println("You're are really old.")
     }
-    //can also use for loop to print list items
-//    for(i in mutableList) {
-//        println(i)
+
+    val string = readLine()
+    when(string) {
+        "hello" -> println("How are you?")
+    }
+
+    //if there is another variable which needs to be compared then we can't use when statement anymore because when statement is dependent on only single variable
+    when(age) {
+        in 0..5 -> println("You're a young kid.")
+//        println("Another line.") //can't use more than two statements. Note: use {} for that
+        in 6..17 -> println("You're a teenager.")
+//        18 && x==6 -> println("You are 18") //error: can't use two variables
+        18 -> if (x == 6) {
+            println("Finally, you're 18")
+        }
+
+        19, 20 -> {
+            println("You're a young adult.")
+            println("Another line.")
+        }
+        in 21..65 -> println("YOu're an adult.")
+        else -> println("You're are really old.")
+    }
+
+
+
+
+    //solving problem using if statements
+//    if(age in 0..5) {
+//        println("You're a young kid.")
+//    } else if (age in 6..17) {
+//        println("You're a teenager")
+//    } else if(age == 18) {
+//        println("Finally, you're 18!")
+//    } else if(age == 19 || age == 20) {
+//        println("You're are a young adult.")
+//    } else if(age in 21..65) {
+//        println("You're an adult.")
+//    } else {
+//        println("You're are really old.")
 //    }
-    println(mutableList)
 
 
-
-    //practice task: input 5 numbers from user and print them in reverse order
-    val mutableList2 = mutableListOf<Int>()
-    println("Enter 5 numbers one by one: ")
-    for(i in 1..5) {
-        val number = readLine()?.toInt()
-        if(number != null) {
-            mutableList2.add(number)
-        }
+    //practice task: take input from user as Country name and then greet in that way.
+    val countryName = readLine()
+    when(countryName){
+        "Pakistan" -> println("Assalam-o-alaikum")
+        "USA" -> println("Hello")
+        "Germany" -> println("Hallo")
+        "Russia" -> println("privet")
+        else -> println("I don't know Greetings of that country.")
     }
-    for(i in mutableList2.size - 1 downTo 0) {
-        println(mutableList2[i])
-    }
-
-
-    //practice task: input number larger than 1 and then generate fibonacci series up to that number
-    val mutableList3 = mutableListOf(0, 1)
-    println("Enter a number n > 1: ")
-    val number = readLine()?.toInt()
-    if(number != null) {
-        var nextNumber = 0
-        for(i in 2..number-1) {
-            nextNumber = mutableList3[i-1] + mutableList3[i-2]
-            mutableList3.add(nextNumber)
-        }
-    }
-    println(mutableList3)
-
 }
