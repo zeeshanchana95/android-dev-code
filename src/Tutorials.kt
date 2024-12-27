33757878f83ba2f9d806e4b762d2ce6793d720a5
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-27 21:07:10 +0500

    function with return value

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,30 +1,45 @@
 import java.io.IO.println
 
 fun main() {
-    printThreeLines()
-    printPow(3, 5)
-    printPow(5,2)
-    calculateSum(5)
+    val pow = printPow(3,5)
+    println("3 to the power of 5 is: $pow")
+    println(multiply(3,5))
+
+    val list = listOf(3,2,5,1,5,6,7)
+    val index = returnItemIndex(list, 5)
+    println("The index of 5 is: $index")
 }
 
-fun printPow(base: Int, exponent:Int){
+fun printFirstItem(list:listOf<Int>) = println(list[0])
+
+//single line functions
+fun multiply(a:Int, b:Int) = a * b
+
+fun printPow(base: Int, exponent:Int):Int {
     var result = 1
     for(i in 1..exponent) {
         result *= base
     }
-    println("$base to the power of $exponent is: $result")
+    return result
+//    println("after") //ignored
 }
 
-fun printThreeLines() {
-    println("First")
-    println("Second")
-    println("Third")
+
+// practice task: given a list with items. user give item to search from list and program will return index of item.
+fun returnItemIndex(list:listOf<Int>, searchedItem:Int):Int {
+    for (item in 0..list.size - 1) {
+        if(searchedItem == list[item]) {
+            return item
+        }
+    }
 }
 
-fun calculateSum(num:Int) {
+
+//practice task
+fun calculateSum(num:Int):Int {
     var sum = 0
     for(i in 1..num) {
         sum += num
     }
-    println("The sum of numbers from 1 to $num is: $sum")
+    return sum
 }
