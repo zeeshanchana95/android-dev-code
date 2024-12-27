9ad27b681c79a8e0964d5413d9d58eb6c70a1a53
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-27 21:54:37 +0500

    extension functions in Kotlin

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,51 +1,41 @@
+import java.io.IO.print
 import java.io.IO.println
 
 fun main() {
-    val array = intArrayOf(10,20,25)
-    val max = getMax(1,2,7,5,3, *array,4,6,1,10,15,4)
-    println("The maximum is $max")
-    searchFor("How to become a good programmer.")
-    searchFor(search = "How to become the best Kotlin Programmer.", searchEngine = "Bing")
-    searchFor(searchEngine = "Bing", search = "How to become best Kotlin programmer.")
+    println("Please enter a number")
+    val input = readLine()?.toInt()
 
-    val sum = alternatingSum(3,4,5,2,1,2,3)
-    println("The alternating sum is $sum")
-}
-
-//vararg:
-    //- used to accept one or more than one input from user
-    //- used to insert array items in between to our vararg elements
-
-//default arguments:
-    //- set initial value of an argument during function declaration
-
-//named Parameters or Named Arguments:
-    //- used for our easiness if we have lots of parameters
-    //- you don't need to remember its order but just use name of parameter and then assign value to it.
-
-
-fun searchFor(search: String, searchEngine:String = "Google") {
-    println("Searching for $search on $searchEngine")
-}
-fun getMax(vararg numbers:Int):Int {
-    var max = numbers[0]
-    for(number in numbers) {
-        if(number > max) {
-            max = number
-        }
-    }
-    return max
-}
-
-//homework: create a function named "alternatingSum" that takes vararg parameter and the function should return the alternating sum (firstNumber - secondNumber + thirdNumber - fourthNumber + etc. of the numbers
-fun alternatingSum(vararg numbers:Int):Int {
-    var alternativeSum = 0
-    for(i in 0..numbers.size - 1) {
-        if(i %2 == 0) {
-            alternativeSum += numbers[i]
+    if(input != null) {
+        if(input.isPrime()) {
+            println("$input is a prime number.")
         } else {
-            alternativeSum -= numbers[i]
+            println("$input is not a prime number.")
         }
     }
-    return alternativeSum;
+}
+
+//extension functions:
+    //we don't need to specify what is its type
+fun Int.isPrime(number:Int):Boolean {
+    for (i in 2 until this - 1) {
+        if(this % i == 0) {
+            return false
+        }
+    }
+    return true
+}
+
+
+fun alternatingSum(vararg numbers:Int):Int {
+    var sum = 0
+    var toggle = true
+    for(number in numbers) {
+        if(toggle) {
+            sum += number
+        } else {
+            sum -= number
+        }
+        toggle = !toggle
+    }
+    return sum
 }
