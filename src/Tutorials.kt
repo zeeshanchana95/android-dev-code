aed9919fd1ab488934172402ad20a789e8cf2fd1
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-28 14:06:46 +0500

    practice task of list and array

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,28 +1,26 @@
 import java.io.IO.println
 
 fun main() {
-    val rect = Rectangle(5.0)
-    val circle = Circle(3.0)
-    val triangle = Triangle(7.0,7.0,7.0)
+    val list = listOf(1,2,3,4,5)
+    val array = arrayOf(1,2,3,4,5)
 
-    val maxAreaRectAndCircle = maxArea(rect, circle)
-    val maxAreaRectCircleTriangle = maxArea(rect, circle, triangle)
+    println(list)
 
-    println("The maximum area of the rectangle and the circle is: $maxAreaRectAndCircle")
-    println("The maximum area of the rectangle, the circle and triangle is: $maxAreaRectCircleTriangle")
-
+    printAlternately(array)
 }
 
-fun maxArea(shape1:Shape, shape2:Shape):Double {
-    val areaShape1 = shape1.area()
-    val areaShape2 = shape2.area()
-    return if(areaShape1> areaShape2) areaShape1 else areaShape2
+fun printAlternately(list: List<Int>) {
+    var i = 0
+    var j = list.size - 1
+    var toggle = true
+    while (i <= j) {
+        if(toggle) {
+            println(list[i])
+            i++
+        } else {
+            println(list[j])
+            j--
+        }
+        toggle = !toggle
+    }
 }
-
-fun maxArea(shape1:Shape, shape2:Shape, shape3:Shape):Double {
-    val maxAreaShape1Shape2 = maxArea(shape1, shape2)
-    val areaShape3 = shape3.area()
-    return if(maxAreaShape1Shape2 > areaShape3) maxAreaShape1Shape2 else areaShape3
-}
-
-
