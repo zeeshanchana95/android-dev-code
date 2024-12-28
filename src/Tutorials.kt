4fb54ebcc25fbdc3c337228a0a1d602d64150f5d
Zeeshan Ahmed Channa <zeeshanchana95@gmail.com>
2024-12-28 08:53:00 +0500

    classes and objects in Kotlin

--- src/Tutorials.kt
+++ src/Tutorials.kt
@@ -1,41 +1,12 @@
-import java.io.IO.print
 import java.io.IO.println
 
 fun main() {
-    println("Please enter a number")
-    val input = readLine()?.toInt()
 
-    if(input != null) {
-        if(input.isPrime()) {
-            println("$input is a prime number.")
-        } else {
-            println("$input is not a prime number.")
-        }
-    }
-}
+    //Class:
+        //- it is basically a blueprint of an object
 
-//extension functions:
-    //we don't need to specify what is its type
-fun Int.isPrime(number:Int):Boolean {
-    for (i in 2 until this - 1) {
-        if(this % i == 0) {
-            return false
-        }
-    }
-    return true
-}
+    val x:Int = 0
+    val y:String = ""
+    //x and y are objects and Int and String are classes
 
-
-fun alternatingSum(vararg numbers:Int):Int {
-    var sum = 0
-    var toggle = true
-    for(number in numbers) {
-        if(toggle) {
-            sum += number
-        } else {
-            sum -= number
-        }
-        toggle = !toggle
-    }
-    return sum
 }
